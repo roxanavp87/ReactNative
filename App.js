17bdefa28298f50135f4fb0e20b484f6e4da7162
roxanavp87 <roxanavp87@gmail.com>
2018-03-11 19:20:37 -0500

    first app

--- App.js
+++ App.js
@@ -1,47 +1,84 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, View, TextInput, Button, Alert, TouchableHighlight } from 'react-native';
-import Toggle from './Toggle'
+import { AppRegistry, StyleSheet, Text, View, TextInput, Button, Alert, TouchableHighlight, Image } from 'react-native';
+
+const Images = [
+  {
+    uri: "https://i.imgur.com/mxgtWKt.jpg",
+    label: "Cat on a blue blanket"
+  },
+
+  {
+    uri: "https://i.imgur.com/XCRnNWn.jpg",
+    label: "A cat toy"
+  },
+
+  {
+    uri: "https://i.imgur.com/dqQX1K0.jpg",
+    label: "A close up of a dog"
+  },
+
+  {
+    uri: "https://i.imgur.com/nZXbSbh.jpg",
+    label: "Sheep next to a cat"
+  },
+
+  {
+    uri: "https://i.imgur.com/mXCjefR.jpg",
+    label: "Cat laying on the grass"
+  },
+
+  {
+    uri: "https://i.imgur.com/AGyxRcc.jpg",
+    label: "Bird sitting on a railing"
+  }
+];
 
 export default class App extends React.Component {
-
   state = {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    index: 0,
+    imageWidth: null
+  }
+
+  onImageLayout(event) {
+    this.setState({
+      imageWidth: event.nativeEvent.layout.width
+    })
+  }
+
+  nextImage(event) {
+    let newIndex = (this.state.index == Images.length-1) ? 0 : this.state.index + 1
+    this.setState({
+      index: newIndex
+    })
+  }
+
+  previousImage(event) {
+    let newIndex = (this.state.index == 0) ? Images.length -1 : this.state.index - 1
+    this.setState({
+      index: newIndex
+    })
   }
 
   render() {
-    const {flexDirection, alignItems, justifyContent} = this.state
-    const layoutStyle = {flexDirection, justifyContent, alignItems}
-
-    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
+    const image = Images[this.state.index];
 
     return (
       <View style={styles.container}>
-        <Toggle
-          label={'Primary axis (flexDirection)'}
-          value={flexDirection}
-          options={['row', 'column']}
-          onChange={(option) => this.setState({flexDirection: option})}
-        />
-        <Toggle
-          label={primaryAxis + ' distribution (justifyContent)'}
-          value={justifyContent}
-          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
-          onChange={(option) => this.setState({justifyContent: option})}
-        />
-        <Toggle
-          label={secondaryAxis + ' alignment (alignItems)'}
-          value={alignItems}
-          options={['flex-start', 'center', 'flex-end', 'stretch']}
-          onChange={(option) => this.setState({alignItems: option})}
-        />
-        <View style={[styles.layout, layoutStyle]}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
+        <View style={styles.btnsContainer} >
+          <Button onPress={this.previousImage.bind(this)} title="Previous" />
+          <Button onPress={this.nextImage.bind(this)} title="Next" />
         </View>
+        
+          
+        <TouchableHighlight onPress={() => { Alert.alert('Awesome!')}} style={styles.image} >
+          <Image source={{ uri: image.uri }}
+            style={styles.image}
+            onLayout={this.onImageLayout.bind(this)} >
+          </Image>
+        </TouchableHighlight>
+          
+
+        <View style={styles.empty} />
       </View>
     )
   }
@@ -50,14 +87,29 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#abcdef',
   },
-  layout: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.05)',
+  btnsContainer: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 35
   },
-  box: {
-    padding: 25,
-    backgroundColor: 'steelblue',
-    margin: 5,
+  image: {
+    flex: 4,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
-})
+  imageLabel: {
+    textAlign: 'center',
+    backgroundColor: 'rgba(100, 100, 100, 0.5)',
+    color: 'white',
+    width: 320
+  },
+  empty: {
+    flex: 1
+  }
+});
